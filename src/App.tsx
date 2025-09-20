ffe9ee8848199608392b823434fdfa46410c8e3b
nezh <randomnameiscoolokay@gmail.com>
2025-09-20 17:18:19 +1000

    Add generateNextGeneration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
+import { generateNextGeneration } from './generation'
 import { Viewport } from './Viewport'
 
-const generation = new Set<string>()
-
-generation.add('0,0')
-generation.add('1,1')
-generation.add('4,7')
-
 export function App() {
+  const [generation, setGeneration] = useState<Set<string>>(new Set([
+    '5,5',
+    '5,6',
+    '5,4'
+  ]))
+
   return (
     <>
       <h1>Conway's Game of Life</h1>
 
       <Viewport generation={generation} />
+
+      <button onClick={() => setGeneration(generateNextGeneration(generation))}>
+        Next Generation
+      </button>
     </>
   )
 }
---
+++ src/generation.ts
@@ -0,0 +1,31 @@
+export function generateNextGeneration(currentGeneration: Set<string>): Set<string> {
+  const neighborCounts = new Map<string, number>()
+  currentGeneration.forEach((cell) => {
+    const [x, y] = cell.split(',').map(Number)
+
+    for (let dx = -1; dx <= 1; dx++) {
+      for (let dy = -1; dy <= 1; dy++) {
+        if (dx === 0 && dy === 0) {
+          continue
+        }
+
+        const neighbor = `${x + dx},${y + dy}`
+        const count = neighborCounts.get(neighbor) || 0
+        neighborCounts.set(neighbor, count + 1)
+      }
+    }
+  })
+
+  const nextGeneration = new Set<string>()
+  neighborCounts.forEach((count, cell) => {
+    if (count === 3 || (count === 2 && currentGeneration.has(cell))) {
+      nextGeneration.add(cell)
+    }
+  })
+
+  console.log(currentGeneration)
+  console.log(neighborCounts)
+  console.log(nextGeneration)
+
+  return nextGeneration
+}
