7dddaa589283e96396388b3ddcceee60e9f52ce9
nezh <randomnameiscoolokay@gmail.com>
2025-09-20 17:01:49 +1000

    Add render (Viewport)

---
+++ src/Viewport.tsx
@@ -0,0 +1,32 @@
+type CellProps = {
+  cell: string
+}
+
+const SIZE = 20
+
+function Cell({cell}: CellProps) {
+  const [x, y] = cell.split(',').map(Number)
+  return (
+    <div
+      className="cell"
+      style={{
+        left: x * SIZE,
+        top: y * SIZE,
+      }}
+    />
+  )
+}
+
+type ViewportProps = {
+  generation: Set<string>
+}
+
+export function Viewport({generation}: ViewportProps) {
+  return (
+    <div className="viewport">
+      {Array.from(generation).map((cell) => (
+        <Cell key={cell} cell={cell} />
+      ))}
+    </div>
+  )
+}
